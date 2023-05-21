5d26da17853eedb44c93f95f1cb496290820b825
RakibulArtiSoft <rakibul@artificial-soft.com>
2023-05-21 16:45:26 +0600

    container advance
    dasboard screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,84 +1,137 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(DashboardApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-// This widget is
-//the root of your application.
+class DashboardApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'ClipOval',
+      title: 'Dashboard App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const MyHomePAGE(),
-      debugShowCheckedModeBanner: false,
+      home: DashboardScreen(),
     );
   }
 }
 
-class MyHomePAGE extends StatefulWidget {
-  const MyHomePAGE({Key? key}) : super(key: key);
-
-  @override
-// ignore: library_private_types_in_public_api
-  _MyHomePAGEState createState() => _MyHomePAGEState();
-}
-
-class _MyHomePAGEState extends State<MyHomePAGE> {
+class DashboardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('GeeksforGeeks'),
-        backgroundColor: Colors.green,
+        title: Text('Dashboard'),
       ),
-      body: Center(
-          child: RichText(
-            // Controls visual overflow
-            overflow: TextOverflow.clip,
-
-            // Controls how the text should be aligned horizontally
-            textAlign: TextAlign.end,
-
-            // Control the text direction
-            textDirection: TextDirection.rtl,
-
-            // Whether the text should break at soft line breaks
-            softWrap: true,
-
-            // Maximum number of lines for the text to span
-            maxLines: 1,
-
-            // The number of font pixels for each logical pixel
-            textScaleFactor: 1,
-            text: TextSpan(
-              text: 'Hello ',
-              style: DefaultTextStyle.of(context).style,
-              children: const <TextSpan>[
-                TextSpan(
-                    text: 'Geeks', style: TextStyle(fontWeight: FontWeight.bold)),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            // First row with two items
+            Row(
+              children: [
+                Expanded(
+                  child: DashboardItem(
+                    icon: Icons.account_circle,
+                    title: 'Users',
+                    count: 120,
+                  ),
+                ),
+                SizedBox(width: 16.0),
+                Expanded(
+                  child: DashboardItem(
+                    icon: Icons.shopping_cart,
+                    title: 'Orders',
+                    count: 80,
+                  ),
+                ),
               ],
             ),
-          )),
-      backgroundColor: Colors.lightBlue[50],
+            SizedBox(height: 16.0),
+            // Second row with one item
+            Expanded(
+              child: DashboardItem(
+                icon: Icons.insert_chart,
+                title: 'Sales',
+                count: 500,
+              ),
+            ),
+            SizedBox(height: 16.0),
+            // Third row with two items
+            Row(
+              children: [
+                Expanded(
+                  child: DashboardItem(
+                    icon: Icons.timeline,
+                    title: 'Analytics',
+                    count: 250,
+                  ),
+                ),
+                SizedBox(width: 16.0),
+                Expanded(
+                  child: DashboardItem(
+                    icon: Icons.settings,
+                    title: 'Settings',
+                    count: 0,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
 
-class MyClip extends CustomClipper<Rect> {
-  @override
-  Rect getClip(Size size) {
-    return const Rect.fromLTWH(0, 0, 100, 100);
-  }
+class DashboardItem extends StatelessWidget {
+  final IconData icon;
+  final String title;
+  final int count;
+
+  DashboardItem({
+    required this.icon,
+    required this.title,
+    required this.count,
+  });
 
   @override
-  bool shouldReclip(oldClipper) {
-    return false;
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.2),
+            spreadRadius: 2,
+            blurRadius: 4,
+            offset: Offset(0, 2),
+          ),
+        ],
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, size: 48.0),
+          SizedBox(height: 8.0),
+          Text(
+            title,
+            style: TextStyle(
+              fontSize: 18.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 4.0),
+          Text(
+            count.toString(),
+            style: TextStyle(fontSize: 16.0),
+          ),
+        ],
+      ),
+    );
   }
 }
